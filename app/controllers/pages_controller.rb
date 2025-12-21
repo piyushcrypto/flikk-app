9bbf06d187c43f31d9546bf3cee1cb2493bcecc3
Piyush Ranjan <piyush.ranjan@planetspark.in>
2025-12-21 06:40:17 +0530

    fixing mobile ui

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,5 +8,20 @@ class PagesController < ApplicationController
     authenticate_user!
     flash.now[:notice] = "Welcome! You are logged in as a #{current_user.role.titleize}."
   end
+
+  def privacy_policy
+  end
+
+  def terms_of_service
+  end
+
+  def content_guidelines
+  end
+
+  def creator_terms
+  end
+
+  def fan_terms
+  end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,13 @@ Rails.application.routes.draw do
   root 'pages#landing'
   get 'dashboard', to: 'pages#dashboard', as: :dashboard
 
+  # Legal pages
+  get 'privacy-policy', to: 'pages#privacy_policy', as: :privacy_policy
+  get 'terms-of-service', to: 'pages#terms_of_service', as: :terms_of_service
+  get 'content-guidelines', to: 'pages#content_guidelines', as: :content_guidelines
+  get 'creator-terms', to: 'pages#creator_terms', as: :creator_terms
+  get 'fan-terms', to: 'pages#fan_terms', as: :fan_terms
+
   # Health check
   get 'up' => 'rails/health#show', as: :rails_health_check
 end
