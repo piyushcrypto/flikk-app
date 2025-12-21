ede8df6649bd89a6e1a39e98d0c532b74b2e7aa3
Piyush Ranjan <piyush.ranjan@planetspark.in>
2025-12-21 06:47:06 +0530

    puma changes

--- config/puma.rb
+++ config/puma.rb
@@ -32,7 +32,12 @@ end
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT") { 3000 }
+# Bind to 0.0.0.0 in production for container/cloud environments
+if ENV.fetch("RAILS_ENV", "development") == "production"
+  bind "tcp://0.0.0.0:#{ENV.fetch('PORT') { 3000 }}"
+else
+  port ENV.fetch("PORT") { 3000 }
+end
 
 # Specifies the `environment` that Puma will run in.
 environment rails_env
